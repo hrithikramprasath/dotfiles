85f869acca656ba662f844e6f8a5cab30ee6aff7
Hrithik Ram Prasath <hrithikramprasath@users.noreply.github.com>
2026-09-17 20:52:20 +0100

    fix(fish): add WAYLAND_DISPLAY safeguard to auto-Niri fallback

--- .config/fish/auto-Niri.fish
+++ .config/fish/auto-Niri.fish
@@ -1,5 +1,5 @@
-# Auto start Niri on tty1
-if test -z "$DISPLAY" ;and test "$XDG_VTNR" -eq 1
+# Auto start Niri on tty1 (fallback for text console)
+if test -z "$DISPLAY" ;and test -z "$WAYLAND_DISPLAY" ;and test "$XDG_VTNR" -eq 1
     mkdir -p ~/.cache
     exec niri-session > ~/.cache/niri.log 2>&1
 end
